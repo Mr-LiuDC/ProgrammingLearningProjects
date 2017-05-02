c9ed3c9d981da18c35d5fae1c59cbd2137db49d4
Mr.LiuDC <442823529@qq.com>
2017-05-02 20:44:01 +0800

    add new file

---
+++ alittler-study-java/src/main/java/cn/alittler/study/java/maven/CleanMvn.java
@@ -0,0 +1,51 @@
+package cn.alittler.study.java.maven;
+
+import java.io.File;
+
+import org.junit.Test;
+
+public class CleanMvn {
+	@Test
+	public void test() {
+		findAndDelete(new File("G:\\.m2\\repository"));
+	}
+
+	public static boolean findAndDelete(File file) {
+		if (!file.exists()) {
+		} else if (file.isFile()) {
+			if (file.getName().endsWith("lastUpdated")) {
+				deleteFile(file.getParentFile());
+				return true;
+			}
+		} else if (file.isDirectory()) {
+			File[] files = file.listFiles();
+			for (File f : files) {
+				if (findAndDelete(f)) {
+					break;
+				}
+			}
+		}
+		return false;
+	}
+
+	public static void deleteFile(File file) {
+		if (!file.exists()) {
+		} else if (file.isFile()) {
+			print("删除文件:" + file.getAbsolutePath());
+			file.delete();
+		} else if (file.isDirectory()) {
+			File[] files = file.listFiles();
+			for (File f : files) {
+				deleteFile(f);
+				// f.delete();
+			}
+			print("删除文件夹:" + file.getAbsolutePath());
+			print("====================================");
+			file.delete();
+		}
+	}
+
+	public static void print(String msg) {
+		System.out.println(msg);
+	}
+}
