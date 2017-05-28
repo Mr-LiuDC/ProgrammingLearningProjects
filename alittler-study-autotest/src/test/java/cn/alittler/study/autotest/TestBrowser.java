bac3628008782aa8658c6d2c5cfcc63bcbcac779
Mr.LiuDC <442823529@qq.com>
2017-05-28 18:06:24 +0800

    update code

--- alittler-study-autotest/src/test/java/cn/alittler/study/autotest/TestBrowser.java
+++ alittler-study-autotest/src/test/java/cn/alittler/study/autotest/TestBrowser.java
@@ -1,20 +1,37 @@
 package cn.alittler.study.autotest;
 
+import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class TestBrowser {
 
-	public static void main(String[] args) throws Exception {
+	@Test
+	public void testChromeDriver() throws Exception {
 		System.setProperty("webdriver.chrome.driver", "./chrome/chromedriver.exe");
-		// WebDriver driver = new FirefoxDriver();
 		WebDriver driver = new ChromeDriver();
 		driver.get("http://www.baidu.com");
 	}
 
+	@Test
+	public void testFirefoxDriver() {
+		System.setProperty("webdriver.gecko.driver", "./gecko/geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://www.baidu.com");
+	}
+	
+	@Test
+	public void test() throws Exception {
+		System.setProperty("webdriver.gecko.driver", "./gecko/geckodriver.exe");
+		System.setProperty("webdriver.chrome.driver", "./chrome/chromedriver.exe");
+		testBrowser(new ChromeDriver());
+		testBrowser(new FirefoxDriver());
+	}
+
 	public static void testBrowser(WebDriver driver) throws Exception {
 		driver.get("http://www.cnblogs.com/tankxiao");
-		Thread.sleep(5000);
+		Thread.sleep(500);
 		// 浏览器最大化
 		driver.manage().window().maximize();
 
