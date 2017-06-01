ed7bb521db490204ee8bd87576e955057ea4ab7a
Decai Liu <decai.liu@hpe.com>
2017-06-01 16:55:44 +0800

    update

--- alittler-study-springboot/src/main/java/cn/alittler/study/springboot/controller/HomeController.java
+++ alittler-study-springboot/src/main/java/cn/alittler/study/springboot/controller/HomeController.java
@@ -1,25 +1,42 @@
 package cn.alittler.study.springboot.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
 @RequestMapping("/")
 public class HomeController {
 
 	@RequestMapping("/")
+	@ResponseBody
 	public String index() {
 		return "Hello World.----这是index";
 	}
 
 	@RequestMapping("/home")
+	@ResponseBody
 	public String home() {
 		return "Hello World.----这是home";
 	}
 
 	@RequestMapping("/hello")
+	@ResponseBody
 	public String hello() {
 		return "Hello World";
+	}
+
+	@RequestMapping("/thymeleaf")
+	public String thymeleaf(ModelMap map) {
+		map.addAttribute("name", "刘德财");
+		return "thymeleaf/thymeleaf";
+	}
+
+	@RequestMapping("/freemarker")
+	public String freeMarker(ModelMap map) {
+		map.addAttribute("name", "刘德财");
+		return "freemarker/freemarker";
 	}
 
 }
---
+++ alittler-study-springboot/src/main/resources/templates/freemarker/freemarker.ftl
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <title>Freemarker模板</title>
+</head>
+<body>
+	Freemarker模板引擎
+	<h1>${name} Hello World</h1>
+</body>
+</html>
